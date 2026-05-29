f9e0a1903a71b93e29b97bd1a75cd3e0fddaa1fe
daneel77 <83504647+daneel77@users.noreply.github.com>
2026-05-29 16:04:12 +0200

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -42,12 +42,15 @@ var lyr_Catastro_2 = new ol.layer.Image({
 wms_layers.push([lyr_Catastro_2, 0]);
 
 // =========================================================================
-// DIVISIÓN EN 5 CATEGORÍAS INDEPENDIENTES PARA LA LEYENDA (MISMOS ESTILOS)
+// LECTURA DEL GEOJSON ORIGINAL (UNA SOLA VEZ)
 // =========================================================================
 var format_EibCcl_Numeracion_Catastro_3 = new ol.format.GeoJSON();
 var features_EibCcl_Numeracion_Catastro_3 = format_EibCcl_Numeracion_Catastro_3.readFeatures(json_EibCcl_Numeracion_Catastro_3, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
 
+// =========================================================================
+// GRUPO 1: ESTADO DE CONSERVACIÓN
+// =========================================================================
 var source_Bueno = new ol.source.Vector({ attributions: ' ' });
 source_Bueno.addFeatures(features_EibCcl_Numeracion_Catastro_3.filter(function(f) {
     return f.get('ESTADOS_CONS_Estado_Conservacion') === 'Bueno';
@@ -103,16 +106,49 @@ var lyr_Ruina = new ol.layer.Vector({
 var lyr_Vacio = new ol.layer.Vector({
     declutter: false, source: source_Vacio, style: style_EibCcl_Numeracion_Catastro_3, interactive: true,
     maxResolution: resoluciones.maxResolution, minResolution: resoluciones.minResolution,
-    popuplayertitle: 'Numeración Xunqueira de Espadanedo', title: '<img src="styles/legend/ECONS_VACIO.png" /> Sin especificar'
+    popuplayertitle: 'Numeración Xunqueira de Espadanedo', title: '<img src="styles/legend/ECONS_VACIO.png" /> Suelos'
 });
 
-// Agrupamos los checkboxes dentro de su grupo original NUMERACION
-var group_Eib_Numeracion = new ol.layer.Group({
+var group_Eib_ECons = new ol.layer.Group({
     layers: [lyr_Bueno, lyr_Regular, lyr_Malo, lyr_Ruina, lyr_Vacio],
     fold: 'close',
-    title: 'NUMERACION'
+    title: 'ESTADO DE CONSERVACIÓN'
 });
 
+// =========================================================================
+// GRUPO 2: TIPO DE EDIFICACIÓN (FILTRADO POR EN1_TIPO)
+// =========================================================================
+var source_Tipo_N1 = new ol.source.Vector({ attributions: ' ' });
+source_Tipo_N1.addFeatures(features_EibCcl_Numeracion_Catastro_3.filter(function(f) {
+    return f.get('EN1_TIPO') === '00_N1';
+}));
+
+var source_Tipo_N2 = new ol.source.Vector({ attributions: ' ' });
+source_Tipo_N2.addFeatures(features_EibCcl_Numeracion_Catastro_3.filter(function(f) {
+    return f.get('EN1_TIPO') === '00_N2';
+}));
+
+var lyr_Tipo_N1 = new ol.layer.Vector({
+    declutter: false, source: source_Tipo_N1, style: style_EibCcl_Numeracion_Catastro_3, interactive: true,
+    maxResolution: resoluciones.maxResolution, minResolution: resoluciones.minResolution,
+    popuplayertitle: 'Numeración Xunqueira de Espadanedo', title: '<img src="styles/legend/ECONS_VACIO.png" /> Tipo N1'
+});
+
+var lyr_Tipo_N2 = new ol.layer.Vector({
+    declutter: false, source: source_Tipo_N2, style: style_EibCcl_Numeracion_Catastro_3, interactive: true,
+    maxResolution: resoluciones.maxResolution, minResolution: resoluciones.minResolution,
+    popuplayertitle: 'Numeración Xunqueira de Espadanedo', title: '<img src="styles/legend/ECONS_VACIO.png" /> Tipo N2'
+});
+
+var group_Eib_Tipo = new ol.layer.Group({
+    layers: [lyr_Tipo_N1, lyr_Tipo_N2],
+    fold: 'close',
+    title: 'TIPO DE NUMERACIÓN'
+});
+
+// =========================================================================
+// OTROS GRUPOS DEL MAPA ORIGINAL
+// =========================================================================
 var group_Auxiliares = new ol.layer.Group({
     layers: [],
     fold: 'open',
@@ -211,13 +247,15 @@ var group_Eib_Trabajo = new ol.layer.Group({
     title: 'NUCLEOS'
 });
 
-// Visibilidades iniciales
+// Visibilidades iniciales de arranque (Por defecto arrancamos mostrando Estado de Conservación)
 lyr_GoogleSatellite_0.setVisible(true);
 lyr_Ortoimagen_1.setVisible(false);
 lyr_Catastro_2.setVisible(true);
-lyr_Bueno.setVisible(true); lyr_Regular.setVisible(true); lyr_Malo.setVisible(true); lyr_Ruina.setVisible(true); lyr_Vacio.setVisible(true);
 
-var layersList = [group_Varios, group_Eib_Numeracion, group_Eib_Trabajo];
+lyr_Bueno.setVisible(true); lyr_Regular.setVisible(true); lyr_Malo.setVisible(true); lyr_Ruina.setVisible(true); lyr_Vacio.setVisible(true);
+lyr_Tipo_N1.setVisible(false); lyr_Tipo_N2.setVisible(false); // Tipo arranca apagado para ser excluyente
+
+var layersList = [group_Varios, group_Eib_ECons, group_Eib_Tipo, group_Eib_Trabajo];
 
 // =========================================================================
 // CAPA GLOBAL INVISIBLE PARA EL BUSCADOR DE QGIS2WEB
@@ -226,12 +264,12 @@ var source_Completo = new ol.source.Vector({ attributions: ' ' });
 source_Completo.addFeatures(features_EibCcl_Numeracion_Catastro_3);
 var lyr_EibCcl_Numeracion_Catastro_3 = new ol.layer.Vector({ source: source_Completo });
 
-// Asignación en bloque de metadatos, alias y labels a las 5 subcapas
-var subCapas = [lyr_Bueno, lyr_Regular, lyr_Malo, lyr_Ruina, lyr_Vacio];
+// Asignación en bloque de metadatos, alias, labels e interacción
+var subCapas = [lyr_Bueno, lyr_Regular, lyr_Malo, lyr_Ruina, lyr_Vacio, lyr_Tipo_N1, lyr_Tipo_N2];
 subCapas.forEach(function(lyr) {
     lyr.set('fieldAliases', {'fid': 'fid', 'REFCAT': 'REF. CATASTRAL', 'EibCcl_Callejero_eibEntAgp': 'PARROQUIA', 'ENH_CV_00': 'ENH_CV_00', 'ENH_N1_00': 'ENH_N1_00', 'ENH_L1_00': 'ENH_L1_00', 'ENH_MARCA': 'ENH_MARCA', 'ENC_CV_00': 'ENC_CV_00', 'ENC_N1_00': 'ENC_N1_00', 'ENC_L1_00': 'ENC_L1_00', 'ENC_MARCA': 'ENC_MARCA', 'ENA_CV_00': 'ENA_CV_00', 'ENA_N1_00': 'ENA_N1_00', 'ENA_L1_00': 'ENA_L1_00', 'ENA_MARCA': 'ENA_MARCA', 'ENP_CV_00': 'ENP_CV_00', 'ENP_N1_00': 'ENP_N1_00', 'ENP_L1_00': 'ENP_L1_00', 'ENP_MARCA': 'ENP_MARCA', 'END_CV_00': 'END_CV_00', 'END_N1_00': 'NUMERO', 'END_L1_00': 'LETRA', 'END_MARCA': 'END_MARCA', 'EN1_TIPO': 'EN1_TIPO', 'NOMVIA': 'EN1_NOMVIA', 'EN1_PRQ': 'EN1_PRQ', 'EN1_SGVIA': 'EN1_SGVIA', 'Copia_5': 'Copia_5', 'Copia_6': 'Copia_6', 'Copia_7': 'Copia_7', 'Copia_8': 'Copia_8', 'DUPLICADOS': 'DUPLICADOS', 'rotation': 'rotation', 'EibCcl_Callejero_eibTipVia': 'SIGLA', 'EibCcl_Callejero_eibNomVia': 'VIA/LUGAR', 'CV_NUM': 'CV_NUM', 'ID_CV_NA': 'ID_CV_NA', 'ID_CV_NP': 'ID_CV_NP', 'ID_MODIF': 'MODIFICACION', 'ID_CV_ND': 'ID_CV_ND', 'DIRECCION': 'DIRECCION', 'FOTOS_codmun': 'FOTOS_codmun', 'FOTOS_foto_01': 'Foto_01', 'FOTOS_foto_02': 'Foto_02', 'FOTOS_foto_03': 'Foto_03', 'FOTOS_foto_04': 'Foto_04', 'FOTOS_foto_05': 'Foto_05', 'FOTOS_foto_06': 'Foto_06', 'FOTOS_foto_07': 'Foto_07', 'FOTOS_foto_08': 'Foto_08', 'FOTOS_foto_09': 'Foto_09', 'ESTADOS_CONS_Estado_Conservacion': 'Est. Conserv.', });
     lyr.set('fieldImages', {'fid': 'TextEdit', 'REFCAT': 'TextEdit', 'EibCcl_Callejero_eibEntAgp': 'TextEdit', 'ENH_CV_00': 'Range', 'ENH_N1_00': 'Range', 'ENH_L1_00': 'TextEdit', 'ENH_MARCA': 'TextEdit', 'ENC_CV_00': 'Range', 'ENC_N1_00': 'Range', 'ENC_L1_00': 'TextEdit', 'ENC_MARCA': 'TextEdit', 'ENA_CV_00': 'Range', 'ENA_N1_00': 'Range', 'ENA_L1_00': 'TextEdit', 'ENA_MARCA': 'TextEdit', 'ENP_CV_00': 'Range', 'ENP_N1_00': 'Range', 'ENP_L1_00': 'TextEdit', 'ENP_MARCA': 'TextEdit', 'END_CV_00': 'Range', 'END_N1_00': 'Range', 'END_L1_00': 'TextEdit', 'END_MARCA': 'TextEdit', 'EN1_TIPO': 'TextEdit', 'EN1_NOMVIA': 'TextEdit', 'EN1_PRQ': 'TextEdit', 'EN1_SGVIA': 'TextEdit', 'Copia_5': 'TextEdit', 'Copia_6': 'TextEdit', 'Copia_7': 'TextEdit', 'Copia_8': 'TextEdit', 'DUPLICADOS': 'TextEdit', 'rotation': 'TextEdit', 'EibCcl_Callejero_eibTipVia': 'TextEdit', 'EibCcl_Callejero_eibNomVia': 'TextEdit', 'CV_NUM': 'TextEdit', 'ID_CV_NA': 'Range', 'ID_CV_NP': 'Range', 'ID_MODIF': 'TextEdit', 'ID_CV_ND': 'Range', 'DIRECCION': 'TextEdit', 'FOTOS_codmun': 'TextEdit', 'FOTOS_foto_01': 'TextEdit', 'FOTOS_foto_02': 'TextEdit', 'FOTOS_foto_03': 'TextEdit', 'FOTOS_foto_04': 'TextEdit', 'FOTOS_foto_05': 'TextEdit', 'FOTOS_foto_06': 'TextEdit', 'FOTOS_foto_07': 'TextEdit', 'FOTOS_foto_08': 'TextEdit', 'FOTOS_foto_09': 'TextEdit', 'ESTADOS_CONS_Estado_Conservacion': '', });
-    lyr.set('fieldLabels', {'fid': 'hidden field', 'REFCAT': 'inline label - always visible', 'EibCcl_Callejero_eibEntAgp': 'inline label - always visible', 'ENH_CV_00': 'hidden field', 'ENH_N1_00': 'hidden field', 'ENH_L1_00': 'hidden field', 'ENH_MARCA': 'hidden field', 'ENC_CV_00': 'hidden field', 'ENC_N1_00': 'hidden field', 'ENC_L1_00': 'hidden field', 'ENC_MARCA': 'hidden field', 'ENA_CV_00': 'hidden field', 'ENA_N1_00': 'hidden field', 'ENA_L1_00': 'hidden field', 'ENA_MARCA': 'hidden field', 'ENP_CV_00': 'hidden field', 'ENP_N1_00': 'hidden field', 'ENP_L1_00': 'hidden field', 'ENP_MARCA': 'hidden field', 'END_CV_00': 'hidden field', 'END_N1_00': 'inline label - always visible', 'END_L1_00': 'inline label - always visible', 'END_MARCA': 'hidden field', 'EN1_TIPO': 'inline label - always visible', 'EN1_NOMVIA': 'hidden field', 'EN1_PRQ': 'hidden field', 'EN1_SGVIA': 'hidden field', 'Copia_5': 'hidden field', 'Copia_6': 'hidden field', 'Copia_7': 'hidden field', 'Copia_8': 'hidden field', 'DUPLICADOS': 'hidden field', 'rotation': 'hidden field', 'EibCcl_Callejero_eibTipVia': 'inline label - always visible', 'EibCcl_Callejero_eibNomVia': 'inline label - always visible', 'CV_NUM': 'hidden field', 'ID_CV_NA': 'hidden field', 'ID_CV_NP': 'hidden field', 'ID_MODIF': 'inline label - always visible', 'ID_CV_ND': 'hidden field', 'DIRECCION': 'inline label - always visible', 'FOTOS_codmun': 'hidden field', 'FOTOS_foto_01': 'inline label - visible with data', 'FOTOS_foto_02': 'inline label - visible with data', 'FOTOS_foto_03': 'inline label - visible with data', 'FOTOS_foto_04': 'inline label - visible with data', 'FOTOS_foto_05': 'inline label - visible with data', 'FOTOS_foto_06': 'inline label - visible with data', 'FOTOS_foto_07': 'inline label - visible with data', 'FOTOS_foto_08': 'inline label - visible with data', 'FOTOS_foto_09': 'inline label - visible with data', 'ESTADOS_CONS_Estado_Conservacion': 'inline label - visible with data', });
+    lyr.set('fieldLabels', {'fid': 'hidden field', 'REFCAT': 'inline label - always visible', 'EibCcl_Callejero_eibEntAgp': 'inline label - always visible', 'ENH_CV_00': 'hidden field', 'ENH_N1_00': 'hidden field', 'ENH_L1_00': 'hidden field', 'ENH_MARCA': 'hidden field', 'ENC_CV_00': 'hidden field', 'ENC_N1_00': 'hidden field', 'ENC_L1_00': 'hidden field', 'ENC_MARCA': 'hidden field', 'ENA_CV_00': 'hidden field', 'ENA_N1_00': 'hidden field', 'ENA_L1_00': 'hidden field', 'ENA_MARCA': 'hidden field', 'ENP_CV_00': 'hidden field', 'ENP_N1_00': 'hidden field', 'ENP_L1_00': 'hidden field', 'ENP_MARCA': 'hidden field', 'END_CV_00': 'hidden field', 'END_N1_00': 'inline label - always visible', 'END_L1_00': 'inline label - always visible', 'END_MARCA': 'hidden field', 'EN1_TIPO': 'inline label - always visible', 'EN1_NOMVIA': 'hidden field', 'EN1_PRQ': 'hidden field', 'EN1_SGVIA': 'hidden field', 'Copia_5': 'hidden field', 'Copia_6': 'hidden field', 'Copia_7': 'hidden field', 'Copia_8': 'hidden field', 'DUPLICADOS': 'hidden field', 'rotation': 'hidden field', 'EibCcl_Callejero_eibTipVia': 'inline label - always visible', 'EibCcl_Callejero_eibNomVia': 'inline label - always visible', 'CV_NUM': 'hidden field', 'ID_CV_NA': 'hidden field', 'ID_CV_NP': 'hidden field', 'ID_MODIF': 'hidden field', 'ID_CV_ND': 'hidden field', 'DIRECCION': 'inline label - always visible', 'FOTOS_codmun': 'hidden field', 'FOTOS_foto_01': 'inline label - visible with data', 'FOTOS_foto_02': 'inline label - visible with data', 'FOTOS_foto_03': 'inline label - visible with data', 'FOTOS_foto_04': 'inline label - visible with data', 'FOTOS_foto_05': 'inline label - visible with data', 'FOTOS_foto_06': 'inline label - visible with data', 'FOTOS_foto_07': 'inline label - visible with data', 'FOTOS_foto_08': 'inline label - visible with data', 'FOTOS_foto_09': 'inline label - visible with data', 'ESTADOS_CONS_Estado_Conservacion': 'inline label - visible with data', });
     
     lyr.on('precompose', function(evt) {
         evt.context.globalCompositeOperation = 'normal';
@@ -242,10 +280,38 @@ lyr_Eib_Lugares_0.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
 
-// Control estricto del orden del renderizado en pantalla (Z-Index)
 lyr_Eib_Lugares_0.setZIndex(10);
-lyr_Bueno.setZIndex(100);
-lyr_Malo.setZIndex(100);
-lyr_Regular.setZIndex(100);
-lyr_Ruina.setZIndex(100);
-lyr_Vacio.setZIndex(100);
+lyr_Bueno.setZIndex(100); lyr_Regular.setZIndex(100); lyr_Malo.setZIndex(100); lyr_Ruina.setZIndex(100); lyr_Vacio.setZIndex(100);
+lyr_Tipo_N1.setZIndex(100); lyr_Tipo_N2.setZIndex(100);
+
+// =========================================================================
+// SCRIPT DE EXCLUSIVIDAD MUTUA EN LEYENDA (AUTOMÁTICO)
+// =========================================================================
+var bloqueandoEventos = false;
+
+var capasConservacion = [lyr_Bueno, lyr_Regular, lyr_Malo, lyr_Ruina, lyr_Vacio];
+var capasTipo = [lyr_Tipo_N1, lyr_Tipo_N2];
+
+// Evento: Al activar algo en Conservación, se apaga Tipo
+capasConservacion.forEach(function(capa) {
+    capa.on('change:visible', function() {
+        if (bloqueandoEventos) return;
+        if (capa.getVisible()) {
+            bloqueandoEventos = true;
+            capasTipo.forEach(function(c) { c.setVisible(false); });
+            bloqueandoEventos = false;
+        }
+    });
+});
+
+// Evento: Al activar algo en Tipo, se apaga Conservación
+capasTipo.forEach(function(capa) {
+    capa.on('change:visible', function() {
+        if (bloqueandoEventos) return;
+        if (capa.getVisible()) {
+            bloqueandoEventos = true;
+            capasConservacion.forEach(function(c) { c.setVisible(false); });
+            bloqueandoEventos = false;
+        }
+    });
+});
